e3536f6239e4b401307d4a6aa7e741628d9d6c08
petrullo <agpetrullo@gmail.com>
2020-06-18 13:10:15 -0300

    Criados todos endpoints

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -18,9 +18,17 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracoesViewSet
+from avaliacoes.api.viewsets import AvaliacoesViewSet
+from comentarios.api.viewsets import ComentariosViewSet
+from enderecos.api.viewsets import EnderecosViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracoes', AtracoesViewSet)
+router.register(r'avaliacoes', AvaliacoesViewSet)
+router.register(r'comentarios', ComentariosViewSet)
+router.register(r'enderecos', EnderecosViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
